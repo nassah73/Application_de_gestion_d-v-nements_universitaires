c2aae7f96a71df984a8a8ef24720936e5935c0a0
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-03-20 17:15:16 +0100

    bg-login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,10 @@
-import {createRoot}from 'react-dom/ .client'
+import {createRoot}from 'react-dom/client'
 import Login from './signin&signup/signin'
+import './style.css'
 const root=createRoot(document.getElementById('root'))
 root.render(
-
+    <>
+   <Login/>
+   <h1>heloo</h1>
+   </>
 )
--- frontend/src/signin&signup/signin.jsx
+++ frontend/src/signin&signup/signin.jsx
@@ -1,7 +1,9 @@
 export default function login(){
     return(
         <div className="container">
-            
+           <div className="bg-login">
+
+           </div>
         </div>
     )
 }
