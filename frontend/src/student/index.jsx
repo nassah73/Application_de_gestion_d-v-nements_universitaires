278de1e880483efb9c0f8964ffb667df6b402979
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-03-25 22:56:07 +0100

    logo

--- frontend/src/student/index.jsx
+++ frontend/src/student/index.jsx
@@ -1,8 +1,17 @@
 import style from './Home.module.css'
+import Bg_img from '../signin&signup/zzzzz-removebg-preview.png'
 export default function Main(){
     return(
         <div className={style.container}>
-            
+            <div>
+                <nav className={style.interface_bar}>
+                    <nav className={style.logo}>
+                        <img src={Bg_img} alt="dashbord logo"  width={'100px'}/>
+                        <p>EventConnect</p>
+                    </nav>
+                   <hr />
+                </nav>
+            </div>
         </div>
     )
 }
