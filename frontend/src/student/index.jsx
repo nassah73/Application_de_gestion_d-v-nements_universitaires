46a0c1ba7f99a6b74ee2670d860b307d11a68f45
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-03-26 19:14:42 +0100

    nav bar

--- frontend/src/student/index.jsx
+++ frontend/src/student/index.jsx
@@ -7,9 +7,26 @@ export default function Main(){
                 <nav className={style.interface_bar}>
                     <nav className={style.logo}>
                         <img src={Bg_img} alt="dashbord logo"  width={'100px'}/>
-                        <p>EventConnect</p>
+                        <p>EventFPT</p>
                     </nav>
+                    
                    <hr />
+                   <nav className={style.list_items}>
+                         <ul >
+                            <li>Home</li>
+                            <li>My Events</li>
+                            <li>Profile</li>
+                            <li>Notification</li>
+                         </ul>
+                    <nav className={style.profile}>
+                        <hr />
+                         <img src={Bg_img} alt="" width={'20px'} className={style.proImg} />
+                         <div className={style.infoPro}>
+                            <h1>Name prename</h1>
+                            <p>student ID :111111</p>
+                         </div>
+                     </nav>      
+                   </nav>
                 </nav>
             </div>
         </div>
