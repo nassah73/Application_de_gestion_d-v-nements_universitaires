a3f437b038d13d722ebf45ecd61e763ee4cdccc6
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-12 17:36:28 +0200

    update main.jsx for my feature

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import {createRoot}from 'react-dom/client'
 import Login from './signin&signup/signin'
 import Admin from './Admin/indix'
-import Home from './student/index'
-
+import Home from './student/inde'
+import './index.css'
 const root=createRoot(document.getElementById('root'))
 root.render(
     <>
-   <Admin/>
+   
+   <Home/>
    
    </>
 )
