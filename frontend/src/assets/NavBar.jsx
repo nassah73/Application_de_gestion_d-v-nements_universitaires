d9d526ba301b4d1b0a08f9a3841d920e3e8566ea
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-12 18:20:25 +0200

    home conetent

--- frontend/src/assets/NavBar.jsx
+++ frontend/src/assets/NavBar.jsx
@@ -3,8 +3,8 @@ import { Bell, UserCircle } from 'lucide-react';
 export default function Navbar(){
       const items=[{name:'events'},{name:'calendar'},{name:'research'},{name:'venues'},{name:'faculty'},{name:'analytics'}]
     return(
-<nav className='bg-white h-20 flex justify-between relative top-5 capitalize font-[600] '>
-                <h1 className='font-[800] uppercase relative left-20 text-[#01223e]'>ibn zohr university</h1>
+<nav className='bg-white h-15 flex justify-between relative top-5 capitalize font-[600] '>
+                <h1 className='font-[800] uppercase relative left-20 '><span className=' text-[#164167]'>fpt</span><span>event</span></h1>
                 <ul className='flex gap-10  text-gray-500'>
                       {items.map((item,index)=>{
                          return(
@@ -15,7 +15,7 @@ export default function Navbar(){
                 <div className=' flex gap-15 relative right-20'>
                      <Bell  size={24} strokeWidth={1.5}/>
                       <UserCircle size={24} strokeWidth={1.5}/>
-                      <button className='bg-blue-500 rounded-xl h-10 relative bottom-2  w-40'>create event</button>
+                      <button className='bg-blue-500 rounded-xl h-10 relative bottom-2  w-40 capitalize'>create event</button>
                 </div>
             </nav>
     )
--- frontend/src/pages/student/inde.jsx
+++ frontend/src/pages/student/inde.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import BgImag from '../../assets/bg.jpg'
 import NavBar from '../../assets/NavBar'
 export default function Main(){
   
     return (
         <div>
           <NavBar/>  
+          <section className='h-screen w-screen bg-bottom relative '  style={{backgroundImage:`url(${BgImag})`} }>
+          <div className="absolute inset-0 bg-black/60"></div>
+                <div className='relative z-10 w-1/3 top-1/8 left-20 '>
+                      <div>
+                        <p className='font-[900] text-7xl uppercase leading-25'><span className='text-[#164167]'>fpt</span> <br/><span >event</span></p>
+                      </div>
+                      
+                      <p className='mt-5 w-[80%] text-white font-[500] '>Say goodbye to paper forms. Join the first centralized digital hub for all  Taroudant academic and cultural activities.</p>
+                </div>
+          </section>
         </div>
     )
 }
