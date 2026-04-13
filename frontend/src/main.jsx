ccdefd7f2856b95c6944ab6259fd8bbe1632591b
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-13 14:47:16 +0200

    event content

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react' 
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'; 
-import App from './App'; 
+{/*import App from './App';*/} 
+import Event from './pages/student/Events'
 import './index.css'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  <React.StrictMode>
+  <>
+  {<React.StrictMode>
     <BrowserRouter>
-      <App />
+  <Event />
+     
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>}
+  </>
 )
--- frontend/src/pages/student/Events.jsx
+++ frontend/src/pages/student/Events.jsx
@@ -9,16 +9,27 @@ export default function Main(){
             <div className="absolute inset-0 bg-black/60" ></div>
             <nav className="bg-amber-800 w-[1300px] mx-auto text-white relative mt-5">
                 <h1 className="font-[600] uppercase text-4xl italic">disopver events</h1>
-                <div className="flex gap-5">
-                    <div className="flex mt-5 gap-1 border-2 w-1/3 py-1.5 rounded-[5px]">
+                <div className="flex justify-around">
+                    <div className="flex mt-5 gap-1 border-2 w-1/3 py-1.5 rounded-[5px] relative">
                      <Search className="text-gray-300 w-10"/>   
                     <form action="">
                         <input className="w-73" type="text" placeholder="Search events,by name ,category,location" />
+                        
                     </form>
                     </div>
-                    <div>
+                    <button className="border-2 relative h-10 px-10 b self-center rounded-[5px] right-1/9 mt-4">Shearch</button>
+                    <div className="self-center">
+                        <form action="">
+                            <select className="h-10 rounded-[5px] text-amber-300 border-2 px-15 mt-4" defaultValue={'category'}>
+                             <option defaultValue={'category'} value="category" disabled>category</option>
+                             <option value="">item3</option>
+                             <option value="">item1</option>
+                             <option value="">item1</option>
+                        </select>
+                        </form>
                         
                     </div>
+                   
                 </div>
             </nav>
          </section>
